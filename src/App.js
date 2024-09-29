5dfd29e5ab60fc233adf4b117d9ea76dae488c87
Kateryna Fastovets <katyafastovets@gmail.com>
2024-09-29 15:38:09 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="div">
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/ " exact className="active">
+                Головна
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" className="active">
+                Про нас
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" className="active">
+                Контакти
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <h1>Це домашня сторінка</h1>;
+}
+
+export default Home;
